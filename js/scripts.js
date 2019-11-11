32e7fef7bd6126c852cce279e5fd331c8b862e81
kolumr <kolumronnie@gmail.com>
2019-11-11 12:44:33 +0300

    Add code to get data from radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
 
-  function getData(){
-    var radioSize = document.getElementById("Radio1");
-    alert(radioSize);
-    var present = 1;
+function getData(){
+  var radioSize = document.getElementsByName("pizza_size");
   for (var i = 0, length = radioSize.length; i < length; i++)
   {
     if (radioSize[i].checked)
     {
       alert(radioSize[i].value);
-    present = 0;
-    
     break;
     }
   }
-  if (present == 1 )
+  var radioSize = document.getElementsByName("crust");
+  for (var i = 0, length = radioSize.length; i < length; i++)
   {
-    alert("Please select Size")
+    if (radioSize[i].checked)
+    {
+      alert(radioSize[i].value);
+    break;
+    }
   }
-  };
+  var radioSize = document.getElementsByName("topping");
+  for (var i = 0, length = radioSize.length; i < length; i++)
+  {
+    if (radioSize[i].checked)
+    {
+      alert(radioSize[i].value);
+    break;
+    }
+  }
+};
