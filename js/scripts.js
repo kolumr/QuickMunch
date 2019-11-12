ce8241496825dc5e7649db45ec8000ae043bbc1d
kolumr <kolumronnie@gmail.com>
2019-11-12 10:36:31 +0300

    Add function to display output

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function getData(){
   var size =0;
   var crust = 0;
   var topping = 0;
+  orderNo = 1;
   var radioSize = document.getElementsByName("pizza_size");
   for (var i = 0, length = radioSize.length; i < length; i++)
   {
@@ -19,6 +20,7 @@ function getData(){
     if (radioCrust[i].checked)
     {
       crust = radioCrust[i].value
+      alert(crust)
     break;
     }
   }
@@ -28,45 +30,82 @@ function getData(){
     if (radioTopping[i].checked)
     {
       topping = radioTopping[i].value
+      // alert(topping)
     break;
     }
   }
-  const pizza1 = new Pizza(size,crust,topping);
+  
+  const pizza1 = new Pizza(size,crust,topping,orderNo,total);
   addToCart(); 
   function addToCart(){
     // const parray = JSON.stringify(pizza1);
-    alert(pizza1.size1);
     if(pizza1.size1 === "Large"){
-      var pizza_size = 1200;
-      alert(pizza_size);
+      pizza_size = 1200;
+      total =total + pizza_size;
+      
     }
     else if(pizza1.size1 === "Medium"){
       pizza_size = 900;
-      alert(pizza_size);
+      total += pizza_size;
     }
-    else{
+    else if(pizza1.size1 === "Small"){
       pizza_size = 700;
-      alert(pizza_size);
-    }
-    if(pizza1.crust1 === 1200){
-      var pizza_size = "Large";
-      alert(pizza_size);
-    }
-    else if(pizza1.size1 === 900){
-      pizza_size = "Medium";
-      alert(pizza_size);
+      total += pizza_size;
     }
     else{
-      pizza_size = "Small";
-      alert(pizza_size);
+      alert("select size");
     }
-  }
+    if(pizza1.crust1 === "Thin crust"){
+      pizza_crust = 150;
+      total += pizza_crust;
+      
+    }
+    else if(pizza1.crust1 === "Thick crust"){
+      pizza_crust = 250;
+      total += pizza_crust;
+    }
+    else if(pizza1.crust1 === "Deep crust"){
+      pizza_crust = 300;
+      total += pizza_crust;
+    }
+    else if(pizza1.crust1 === "Cheese filled crust"){
+      pizza_crust = 250;
+      total += pizza_crust;
+      
+    }
+    else{
+      alert("choose crust ")
+    }
+    if(pizza1.topping1 === "Onions"){
+      pizza_topping = 150;
+      total += pizza_topping;
+      
+    }
+    else if(pizza1.topping1 === "Sausage"){
+      pizza_topping = 200;
+      total += pizza_topping;
+    }
+    else if(pizza1.topping1 === "Bacon"){
+      pizza_topping = 250;
+      total += pizza_topping;
+    }
+    else{
+      // alert("choose topping ")
+    }
+    orderNo =orderNo + 1 ;
+      $("#pizza").append('<tr><th scope="row">'  + pizza1.size1 + '</th><td id="crust">'   + pizza1.crust1 +'</th><td id="topping">'+ pizza1.topping1 + '</th><td id="total">' + total + `</th>`); 
+}
 };
-function Pizza(size,crust,topping){
+function Pizza(size,crust,topping,orderNo,total){
   this.size1 = size;
   this.crust1 = crust;
   this.topping1 = topping;
+  this.orderNo1 = orderNo;
+  this.total1 = total;
   
-}
-
-
+};
+clicks = 0;
+$("button").click(function () {
+  clicks++;
+  addToCart();
+})
